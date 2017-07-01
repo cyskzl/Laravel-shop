111a86d6071d93bc01e10863561503b40d54aa56
dasuan <php_garlic@163.com>
2017-07-01 22:37:21 +0800

    优化首页样式

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -597,7 +597,7 @@
 
 			//导航背景色随频道颜色改变
 			var currentId = $(this).data("currentcategoryid"),navBg;
-
+			// console.log($(this));
 			var img = imgClass(currentId);
 	        $(this).addClass(''+ img +'').css('color','#fff').siblings("a").removeClass().css('color','#626161');
 			// 女士，男士，创意生活切换
