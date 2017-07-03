7d49bf33a9d4ec3e67e19744655a9c033bba01e2
C_YS <C_YS>
2017-07-03 16:26:03 +0800

    adadasd

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -15,23 +15,31 @@ class IndexController extends Controller
         //获取后缀的id 1, 女士默认，2男士,3 生活
         $cateId = $request->route('category_id');
 
-
         //获取最大的分类
         $onecates = Category::where('pid', '=', '0')->get();
         //获取女士 id=1
         $onemaam = self::oneTree(1);
         //获取女士下的2级分类
         $twomaan =  self::twoTree(1);
-//        dd($twomaan);
-//        dd($twomaan);
+
         //获取女士s 的3级分类
         $threemaan = self::threeTree(1);
+        //2级
+        $arr = [];
+        foreach ($twomaan as $v){
+            $twoid = $v->id;
+            $twogoods = Goods::where('cat_id', 'like', '%'.$twoid.'%')->get();
+            $arr[] = $twogoods ;
+//            dump($twogoods);
+        }
+
+        $arr = array_unique($arr);
+        dump($arr);
 
         //获取男士 id=2
         $onemam = self::oneTree(2);
 
         $twomam =  self::twoTree(2);
-
 
         $threeman = self::threeTree(2);
         //获取生活 id=3
@@ -51,7 +59,9 @@ class IndexController extends Controller
         $goodslife= Goods::where('is_new', '=', '1')->where( 'cat_id', 'like', '3%')->orderBy('goods_id','desc')->take(6)->get();
 
         //2级下的商品
-        $goodstwomaams = Goods::where( 'cat_id', 'like', '1_%')->orderBy('goods_id','desc')->take(6)->get();
+        $goodstwomaams = Goods::where( 'cat_id', 'like', '1_%_')->orderBy('goods_id','desc')->take(6)->get();
+
+        $goodsModel = new Goods();
 
         return view('home.index', [
                                 'goodsmaams' => $goodsmaams,
@@ -63,12 +73,19 @@ class IndexController extends Controller
                                 'threelife '=> $threelife ,
                                 'onemaam' => $onemaam ,
                                 'twomaan' =>$twomaan ,
-                                'threemaan' =>$threeman,
+                                'threemaan' =>$threemaan,
                                 'onemam' => $onemam ,
                                 'twoman' =>$twomam ,
                                 'threeman' =>$threeman,
+                                'arr' =>$arr,
+                                'goodsModel'=>$goodsModel,
+                                'goodstwomaams' => $goodstwomaams
 
             ]);
+    }
+    public function commonMethod()
+    {
+
     }
 
     public function oneTree ($id)
