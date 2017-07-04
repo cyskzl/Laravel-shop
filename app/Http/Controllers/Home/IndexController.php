619b540fc3e4578cb871c193d33a647251a153e0
C_YS <C_YS>
2017-07-04 22:40:24 +0800

    前台商品

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -2,105 +2,45 @@
 
 namespace App\Http\Controllers\Home;
 
-use App\Models\Category;
 use App\Models\Goods;
-use Illuminate\Http\Request;
 use App\Http\Requests;
+use App\Models\Category;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    public function index( Request $request,$category_id=1)
+    /**
+     * 返回给前台首页的数据
+     * @param Request $request
+     * @param int $category_id 路由默认值为1，为女的，2为男的
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function homeIndex( Request $request, $category_id = 1)
     {
-//        return $category_id;
+
+        $cateId = $category_id ;
+//        $cateId = $request->session()->get('Index');
+//        dump($cateId);
         //获取后缀的id 1, 女士默认，2男士,3 生活
+        // $cateId = $category_id;
+        return view('home.index', ['request' => $request ,'cateId' => $cateId ]);
 
+    }
+
+    public function cate_Id(Request $request, $category_id = 1)
+    {
         $cateId = $category_id;
-
-        //获取女士 id=1
-        $onemaam = self::oneTree(1);
-        //获取女士下的2级分类
-        $twomaan =  self::twoTree(1);
-//        dd($twomaan);
-//        dd($twomaan);
-        //获取女士s 的3级分类
-        $threemaan = self::threeTree(1);
-
-        //获取男士 id=2
-        $onemam = self::oneTree(2);
-
-        $twomam =  self::twoTree(2);
-
-
-        $threeman = self::threeTree(2);
-        //获取生活 id=3
-        $onelife = self::oneTree(3);
-
-        $twolife = self::twoTree(3);
-
-        $threelife = self::threeTree(3);
-
-        $goodsmaams = Goods::where('is_new', '=', '1')->where( 'cat_id', 'like', '1%')->orderBy('goods_id','desc')->take(6)->get();
-
-        //男士新品
-        $goodsmams = Goods::where('is_new', '=', '1')->where( 'cat_id', 'like', '2%')->orderBy('goods_id','desc')->take(6)->get();
-
-        //生活新品
-        $goodslife= Goods::where('is_new', '=', '1')->where( 'cat_id', 'like', '3%')->orderBy('goods_id','desc')->take(6)->get();
-
-
-        return view('home.index', [
-
-            'goodsmaams' => $goodsmaams,
-            'goodsmams' => $goodsmams,
-            'goodslife' => $goodslife,
-            'cateId' => $cateId,
-            'onelife' => $onelife ,
-            'twolife' => $twolife ,
-            'threelife '=> $threelife ,
-            'onemaam' => $onemaam ,
-            'twomaan' =>$twomaan ,
-            'threemaan' =>$threeman,
-            'onemam' => $onemam ,
-            'twoman' =>$twomam ,
-            'threeman' =>$threeman,
-
-        ]);
-
+        return $cateId;
     }
 
-    public function oneTree ($id)
-    {
-        //获取女士 id=1
-        $data =  Category::where('id', '=', $id )->first();
-        return $data;
-
-    }
-
-    public function twoTree ($id)
-    {
-
-        //获取女士下的2级分类
-        $twodata =  Category::where('level', '=', '0,'.$id)->get();
-        return $twodata;
-    }
-
-    public function threeTree ($pid)
-    {
-
-        //获取女士的3级分类
-        $threedata = Category::where('pid' , '=' , $pid)->get();
-        return $threedata;
-    }
-
-    public function getAjaxNewTree($cateId)
-    {
-        //获取新品商品  1是新品  0不是
-        $newgoods = Goods::where('is_new', '=', '1')->where('cat_id', 'like', $cateId.'%')->orderBy('goods_id','desc')->take(6)->get();
-        return $newgoods;
-
-    }
-
+    /**
+     * 获取首页分类的方法
+     * @param $maxId 1级ID
+     * @param $pid  父ID
+     * @param $cateId 区分男女士
+     * @return array   ajax响应数据
+     */
     public function getAjaxCatetree($maxId, $pid, $cateId)
     {
         //查询这个分类下的商品，4个
@@ -108,22 +48,8 @@ class IndexController extends Controller
 
         $brand = [];
 
-        if($cateId == 1 || $cateId = ''){
-
-            $newgoods = self::getAjaxNewTree($cateId);
-
-            //男士
-        } else if($cateId == 2){
-
-            $newgoods = self::getAjaxNewTree($cateId);
-
-        } else {
-
-            $newgoods = self::getAjaxNewTree($cateId);
-
-        }
-        //新品
         foreach ($goods as $good){
+
             $brand[] = getBrand($good->brand_id);
         }
         $data =   [
@@ -133,12 +59,53 @@ class IndexController extends Controller
             'goods'    => $goods,
             //匹配名称
             'brand'    => $brand,
-            //商品新品
-            'newgoods' => $newgoods
+
         ];
         return $data;
     }
+    /**
+     * 获取1级分类
+     * @param $pid
+     * @return mixed
+     */
+    public function oneTree ($id)
+    {
+        //获取女士 id=1
+        $data =  Category::where('id', '=', $id )->first();
+        return $data;
 
+    }
+
+    /**
+     * 获取2级分类
+     * @param $pid
+     * @return mixed
+     */
+    public function twoTree ($id)
+    {
+
+        //获取女士下的2级分类
+        $twodata =  Category::where('level', '=', '0,'.$id)->get();
+        return $twodata;
+    }
+
+    /**
+     * 获取3级分类
+     * @param $pid
+     * @return mixed
+     */
+    public function threeTree ($pid)
+    {
+        //获取女士的3级分类
+        $threedata = Category::where('pid' , '=' , $pid)->get();
+        return $threedata;
+    }
+
+    /**
+     * 首页分类获取，ajax请求
+     * @param Request $request
+     * @return array
+     */
     public function getAjaxCate(Request $request)
     {
             //接收的2级id
@@ -146,27 +113,68 @@ class IndexController extends Controller
 
             //获取后缀的id 1, 女士默认，2男士,3 生活
             $cateId = $request->input('cate_id');
-//    dd($pid);
+
             //女士
             if($cateId == 1 || $cateId = ''){
 
                 $data = self::getAjaxCatetree('1', $pid, $cateId);
 
               //男士
-            } else if($cateId == 2){
-                echo '123';
-                $data = self::getAjaxCatetree('2', $pid, $cateId);
-
             } else {
 
-                $data = self::getAjaxCatetree('3', $pid, $cateId);
+                $data = self::getAjaxCatetree('2', $pid, $cateId);
 
             }
            return $data;
     }
 
-    public function newgoods()
+    /**
+     * 响应数据给ajax首页新品
+     * @param Request $request
+     * @return array
+     */
+    public function newgoods(Request $request)
     {
+        //接收的2级新品id
+        $pid = $request->input('pid');
+
+        //获取后缀的id 1, 女士默认，2男士,3 生活
+        $cateId = $request->input('cate_id');
+
+        if($cateId == 1){
+
+            $newgoods = self::getAjaxNewTree($cateId);
+
+        } else {
+
+            $newgoods = self::getAjaxNewTree($cateId);
+
+        }
+
+        $brand = [];
+
+        foreach ($newgoods as $newgood){
+            $brand[] = getBrand($newgood->brand_id);
+        }
+
+        return $data = [
+                'brand'    => $brand,
+                'newgoods' => $newgoods
+        ];
 
     }
+
+    /**
+     * 首页获取新品商品
+     * @param $cateId 区分男女士
+     * @return mixed  goods数据
+     */
+    public function getAjaxNewTree($cateId)
+    {
+        //获取新品商品  1是新品  0不是
+        $newgoods = Goods::where('is_new', '=', '1')->where('cat_id', 'like', $cateId.'%')->orderBy('goods_id','desc')->take(6)->get();
+        return $newgoods;
+
+    }
+
 }
