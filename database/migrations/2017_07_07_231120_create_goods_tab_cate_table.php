d0f0c567373f01681b597ee3c635033ad3c5fbc7
dasuan <php_garlic@163.com>
2017-07-08 18:36:50 +0800

    修复购物车BUG

---
+++ database/migrations/2017_07_07_231120_create_goods_tab_cate_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGoodsTabCateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goods_tab_cate', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('cat_id', 11);
+            $table->tinyInteger('is_display',3)->default('0');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('goods_tab_cate');
+    }
+}
