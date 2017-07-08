ed1ac6f35d8e5967e65c08be1e565de0295489ac
lishhPhper <nietzsche_nc@163.com>
2017-07-09 02:40:47 +0800

    合并

--- config/database.php
+++ config/database.php
@@ -111,7 +111,7 @@ return [
         'cluster' => false,
 
         'default' => [
-            'host' => env('REDIS_HOST', 'localhost'),
+            'host' => env('REDIS_HOST', '192.168.35.195'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
             'database' => 0,
