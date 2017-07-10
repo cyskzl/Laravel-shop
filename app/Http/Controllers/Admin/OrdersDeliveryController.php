974d302d536d22e096c82447ad118661adfb99d7
lishhPhper <nietzsche_nc@163.com>
2017-07-10 14:36:20 +0800

    合并

--- app/Http/Controllers/Admin/OrdersDeliveryController.php
+++ app/Http/Controllers/Admin/OrdersDeliveryController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Permission;
+use Illuminate\Support\Facades\DB;
 
 class OrdersDeliveryController extends Controller
 {
